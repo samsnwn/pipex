46ecd65a7fdeb9dd456963430ecbb88d9fa1b2c3
samsnwn <samuel.c.glantz@gmail.com>
2024-10-08 16:02:44 +0200

    more

--- pipex.c
+++ pipex.c
@@ -1,24 +1,61 @@
 #include "pipex.h"
 
-void  pipex(int a, int b, char **argv, char **env)
+void  child_process(int infile, char *cmd, char **env)
+{
+  read(infile)
+}
+
+void  parent_process(int outfile, char *cmd, char **env);
+
+void  pipex(int infile, int outfile, char **argv, char **env)
 {
   int pipe_fds[2];
   int pid;
+  int status;
 
   if (pipe(pipe_fds) == -1)
-    exit(-1);
+    return (perror("Pipe error"));
   pid = fork();
   if (pid == -1)
     return (perror("Fork error"));
   if (pid == 0)
   {
-    // child process
-    child_process(a, argv[1], argv[2], env);
+    // child 1 process
+    // child_process(infile, argv[2], env);
+    // child must read from infile into the input of the first command
+
+    read(infile, pipe_fds[READ_START]);
+    // close(pipe_fds[READ_START]);
+    dup2(pipe_fds[WRITE_END], STDOUT_FILENO);
+    close(pipe_fds[WRITE_END]);
+
+    execve("");
+
+
+
   }
   else
   {
     // parent process
-    parent_process(b, argv[3], argv[4], env);
+    // parent_process(outfile, argv[3], env);
+    close(pipe_fds[WRITE_END]);
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+      // Child 2 process
+      dup2(pipe_fds[READ_START], STDIN_FILENO);
+      close(pipe_fds[READ_START]);
+      dup2(outfile, STDOUT_FILENO);
+      execve();
+    }
+    else 
+    {
+      close(pipe_fds[READ_START]);
+    }
+    wait(&status);
+    wait(&status);
   }
 }
 

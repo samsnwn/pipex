417c1dafc72e088a5fdf1c607e02171ab057940f
samsnwn <samuel.c.glantz@gmail.com>
2024-10-08 10:30:45 +0200

    added pipex

--- pipex.c
+++ pipex.c
@@ -1,41 +1,38 @@
 #include "pipex.h"
 
-int main()
+void  pipex(int a, int b, char **argv, char **env)
 {
-  // argv[1] = infile.txt
-  // argv[2] = command 1
-  // argv[3] = command 2
-  // argv[4] = outfile.txt
-
-  // Parent will create 2 childs with 2 pipes
-  // Parent opens the infile, and pipes the output to child one
-  // child one executes the first command and pipes the outpue to child two
-  // child two executes the second command
-
-
-   //PARENT
-  // int file = open("infile.txt")
-  // dup2(fd[READ_START], file) -> dup takes the output of file descriptor from infile and puts it into read end of the pipe
-
-  //CHILD 1
-  // child 1 reads from READ START of pipe and executes first command exec(argv[2]) 
-  // dup2(fd[], fd[])
-
-  //CHILD 2
-  // child 2 reads from READ START of pipe and executes second command exec(argv[3])
-  // dup2(fd[], fd[])
-
   int pipe_fds[2];
   int pid;
 
-  if (pipe(pipe_fds) == - 1)
+  if (pipe(pipe_fds) == -1)
     exit(-1);
   pid = fork();
-  if (pid < 0)
-    exit(-1);
+  if (pid == -1)
+    return (perror("Fork error"));
   if (pid == 0)
-    child_process();
+  {
+    // child process
+    child_process(a, argv[1], argv[2], env);
+  }
   else
-    parent_process();
-  return 0;
+  {
+    // parent process
+    parent_process(b, argv[3], argv[4], env);
+  }
+}
+
+int main(int argc, char **argv, char **env)
+{
+  int infile;
+  int outfile;
+
+  if (argc != 5)
+    return 1;
+  infile = open(argv[1], O_RDONLY);
+  outfile = open(argv[4], O_CREAT | O_RDWR | O_TRUNC);
+  if (infile < 0 || outfile < 0)
+    return (-1);
+  pipex(infile, outfile, argv, env);
+  return (0);
 }
